8c29950e3dee36bcd98d629de1340db8e3b460b8
Atharva-Gunjal <agunjal750@gmail.com>
2024-12-29 19:53:57 +0530

    Executed

--- practice.java
+++ practice.java
@@ -1,12 +1,10 @@
-public class practice
-{
-public static void main(String args[])
-{
-  int a=1;
-  int b=3;
-  int c;
+public class practice {
+  public static void main(String args[]) {
+    int a = 4;
+    int b = 3;
+    int c;
 
-  c= a + b;
-  System.out.println("Addition is:-" +c);
-}
+    c = a - b;
+    System.out.println("Substraction is:-" + c);
+  }
 }
