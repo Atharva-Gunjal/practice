b60577474baf0d6aba2b62edaea9b66e0c4fea47
Atharva-Gunjal <agunjal750@gmail.com>
2024-12-30 09:21:16 +0530

    exam day

---
+++ exam.java
@@ -0,0 +1,12 @@
+public class exam
+{
+    public static void main(String args[])
+    {
+        int a=10;
+        int b=5;
+        int c;
+
+        c=a+b;
+        System.out.println("addition is:" + c);
+    }
+}
